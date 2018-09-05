32df39e565b0b3e13e7feb57e6e48c1306ada853
shawnchiu <shawnchiu@outlook.com>
2018-09-05 21:31:50 +0800

    完成业务管理页面搭建

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,101 +1,109 @@
-<template style="height: 100%; border: 0px; margin:0px; padding:0px;">
-<el-container style="height: 100%; border: 0px; margin:0px; padding:0px;">
-  <el-container>
-    <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown @command="handleCommand">
-        <i class="el-icon-more" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item command="exit">退出登陆</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>{{loginInfo.name}}</span>
-    </el-header>
-      <el-aside width="200px" >
-    <el-menu @command="go">
-          <el-menu-item index="1">
-        <i class="el-icon-menu"></i>
-        <span slot="title" command="index">首页</span>
-      </el-menu-item>   
-         <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title" command="logQuery">日志查询</span>
-      </el-menu-item>
-         <el-menu-item index="3">
-        <i class="el-icon-menu"></i>
-        <span slot="title" command="realtimeLog">实时监控</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-menu"></i>
-        <span slot="title" command="businessmanager">业务管理</span>
-      </el-menu-item >
-         <el-menu-item index="5"  command="userManager">
-        <i class="el-icon-menu"></i>
-        <span slot="title">用户管理</span>
-      </el-menu-item>
-    </el-menu>
-  </el-aside>
-    <el-main>
-      <Dashboard />
-    </el-main>
-  </el-container>
-</el-container>
+<template style='height: 100%; border: 0px; margin:0px; padding:0px;'>
+
+
+    <el-container class='el-container'>
+        <el-header style='text-align: right; font-size: 12px'>
+            <el-dropdown>
+                <i class='el-icon-setting' style='margin-right: 15px'></i>
+                <el-dropdown-menu slot='dropdown'>
+                    <el-dropdown-item>查看</el-dropdown-item>
+                    <el-dropdown-item>新增</el-dropdown-item>
+                    <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+            <span>王小虎</span>
+        </el-header>
+        <el-container style='height: 100%; border: 1px solid #eee'>
+            <el-aside width='200px;' style='background-color: rgb(238, 241, 246)'>
+                <el-menu class='el-menu-vertical'
+                :default-active='$route.path'
+                router>
+                    <el-menu-item :index='item.router'  v-for='item in menu' :key='item.key' :id='item.key'>
+                        <i :class='item.icon'></i>
+                        <span slot='title'>{{item.name}}</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+
+            <el-main>  <router-view/>
+
+            </el-main>
+        </el-container>
+    </el-container>
 
 </template>
 
 <script>
-  import Dashboard from '../dashboard/Dashboard.vue'
-  
-  export default {
+    import Dashboard from '../dashboard/Dashboard.vue';
 
-  components: {
-    Dashboard
-  },
-   data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
-      return {
-        loginInfo: item,
-        tableData: Array(20).fill(item)
-      }
-    },
-    methods: {
-        handleCommand: function (command) {
-         if ('exit'===command) {
-           alert("已退出");
-         }
-       },
-       go: function (page) {
-         if("index" === page) {
-           alert("已退出");
-         }
-         else if("logQuery" === page) {
-           alert("已退出");
-         }
-         else if("realtimeLog" === page) {
-           alert("已退出");
-         }
-         else if("businessmanager" === page) {
-           alert("已退出");
-         }
-         else if("userManager" === page) {
-           alert("已退出");
-         }
-       }
-    }
-  };
+    export default {
+        components: {
+            Dashboard
+        },
+        data() {
+            const menu = [
+                {
+                    name: '首页',
+                    key: 'index',
+                    index: 1,
+                    icon: 'el-icon-menu',
+                    router: 'index'
+                },
+                {
+                    name: '日志查询',
+                    key: 'logQuery',
+                    index: 2,
+                    icon: 'el-icon-search',
+                    router: 'logQuery'
+                },
+                {
+                    name: '实时监控',
+                    key: 'watch',
+                    index: 3,
+                    icon: 'el-icon-view',
+                    router: 'logWatch'
+                },
+                {
+                    name: '业务管理',
+                    key: 'business',
+                    index: 4,
+                    icon: 'el-icon-tickets',
+                    router: 'business'
+                }, {
+                    name: '用户管理',
+                    key: 'user',
+                    index: 5,
+                    icon: 'el-icon-service',
+                    router: 'user'
+                },
+
+            ];
+            return {
+                menu: menu
+            };
+        }
+    };
 </script>
 
 <style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    color: #333;
-  }
+    .el-header {
+        background-color: #b3c0d1;
+        color: #333;
+        line-height: 60px;
+    }
+
+    .el-aside {
+        color: #333;
+    }
+
+    .el-menu-vertical {
+        height: 100%;
+        width: 200px;
+    }
+
+    .el-container {
+        height: 100%;
+        width: 100%;
+    }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import Dashboard from './components/dashboard/Dashboard.vue';
+import User from './components/user/User.vue';
+import LogQuery from './components/log/LogQuery.vue';
+import Watch from './components/log/Watch.vue';
+import Business from './components/business/Business.vue';
 
 Vue.use(ElementUI);
-Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 };
+Vue.use(VueRouter);
+
+Vue.prototype.$ELEMENT = {size: 'small', zIndex: 3000};
+
+var router = new VueRouter({
+    routes: [
+        {path: '/dashboard', component: Dashboard},
+        {path: '/user', component: User},
+        {path: '/logQuery', component: LogQuery},
+        {path: '/logWatch', component: Watch},
+        {path: '/business', component: Business}
+    ]
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    router: router,
+    render: h => h(App)
 });
